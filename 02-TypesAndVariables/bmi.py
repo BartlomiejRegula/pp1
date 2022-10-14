b01ea2c26ec9d7001a4cc45380ce752879f75754
BartlomiejRegula <bartek.br2003@gmail.com>
2022-10-14 19:44:38 +0200

    zadanie

--- 02-TypesAndVariables/bmi.py
+++ 02-TypesAndVariables/bmi.py
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 wzrost = float(input('podaj wzrost w metrach:\n'))
 masa = int(input('podaj masę w kilogramach:\n'))
 w2= wzrost*wzrost
 bmi = masa/w2
 
 print('bmi wynosi:',bmi)
-=======
-wzrost = input('podaj wzrost\n')
-waga = input('podaj wage\n')
-wzrost = float
-waga=float
-
-bmi = waga/wzrost**2
-
-print(bmi)
->>>>>>> 80e3adc248f0e8bf7f6368c27052da778cc9a959
--- 02-TypesAndVariables/trojkat.py
+++ 02-TypesAndVariables/trojkat.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 a =  int(input('podaj pierwszy bok\n'))
 b =  int(input('podaj drugi bok\n'))
 c =  int(input('podaj trzeci bok\n'))
@@ -9,28 +8,3 @@ s=q/2
 pole=(s*(s-a)*(s-b)*(s-c))**0.5
 
 print('pole wynosi\n', pole)
-=======
-a =  input('podaj pierwszy bok\n')
-b =  input('podaj drugi bok\n')
-c =  input('podaj trzeci bok\n')
-a=float
-b=float
-c=float
-
-q =  (a+b+c)/2
-q=float
-
-
-import math
-
-
-
-qa= q-a
-qb= q-b
-qc= q-c
-qabc= qa*qb*qc
-qw= q*qabc
-w= math.sqrt(qw)
-
-print(w)
->>>>>>> 80e3adc248f0e8bf7f6368c27052da778cc9a959
